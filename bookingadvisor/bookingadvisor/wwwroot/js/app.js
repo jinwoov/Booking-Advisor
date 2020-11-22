1bd405bfe9f0907891aa31b8400262437ae7a4d8
Jin Kim <jinwoov@gmail.com>
2020-11-22 03:35:13 -0600

    added console.log to help the user.

---
+++ bookingadvisor/bookingadvisor/wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+console.log(`%c
+         __    To Mochi the best dog!
+        /  \\
+       / ..|\\
+      (_\\  |_)
+      /  \\@' 
+     /     \\
+_   /  ''   |
+ \\\\/  \\  |_\\\\
+  \\   /_ || \\\\_
+   \\____)|_) \\_)`, "color:#febb02; background:black; font-family: monospace")
+console.log("%cWelcome to the Booking Advisor web application. Goal is to exploit as much as you can!!", "color:#febb02; background:black; font-family: monospace");
+
+function givemehint() {
+    return "All of the challenges within this website should be able to perform within the inspect tool.";
+}
