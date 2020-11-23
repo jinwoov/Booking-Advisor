268c49d1de9fac6afd059ff18d3102cbd52b37e9
Jin Kim <jinwoov@gmail.com>
2020-11-23 10:02:47 -0600

    trying to parse

--- bookingadvisor/bookingadvisor/Models/Currency.cs
+++ bookingadvisor/bookingadvisor/Models/Currency.cs
@@ -7,5 +7,18 @@ namespace bookingadvisor.Models
 {
     public class Currency
     {
+        public  DateTime Date { get; set; }
+        public string Current { get; set; }
+        public Rate[] Rates { get; set; }
+    }
+    public class Rate
+    {
+        public int CAD { get; set; }
+        public int KRW { get; set; }
+        public int PHP { get; set; }
+        public int AUD { get; set; }
+        public int HUF { get; set; }
+        public int IDR { get; set; }
+        public int NZD { get; set; }
     }
 }
--- bookingadvisor/bookingadvisor/Pages/Index.cshtml.cs
+++ bookingadvisor/bookingadvisor/Pages/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace bookingadvisor.Pages
     {
         public void OnGet()
         {
+
         }
     }
 }
